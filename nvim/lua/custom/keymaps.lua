92dd2a533bdad02494f689af916a906aaceab217
cy <cyrod@hey.com>
2025-06-18 11:22:05 +0800

    WIP

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set('v', 'p', '"_dP')
 
 -- Disable annoying command line thing.
 vim.keymap.set('n', 'q:', ':q<CR>')
+
+-- Add jj and jk to ESC
+-- From DevOps Toolbox
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = false })
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = false })
