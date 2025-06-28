8c63616a6dba15673c714acf8a131492a5dca117
cy <cyrod@hey.com>
2025-06-28 14:06:16 +0800

    Add phpactor

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -217,9 +217,33 @@ return {
             },
           },
         },
-        intelephense = {},
+        -- intelephense = {},
         svelte = {},
         tailwindcss = {},
+        phpactor = {
+          capabilities = capabilities,
+          on_attach = function(client, bufnr)
+            client.server_capabilities.implementationProvider = false
+            client.server_capabilities.referencesProvider = false
+            client.server_capabilities.renameProvider = false
+            client.server_capabilities.selectionRangeProvider = false
+            client.server_capabilities.signatureHelpProvider = false
+            client.server_capabilities.typeDefinitionProvider = false
+            client.server_capabilities.workspaceSymbolProvider = false
+            client.server_capabilities.definitionProvider = false
+            client.server_capabilities.documentHighlightProvider = false
+            client.server_capabilities.documentSymbolProvider = false
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
+          init_options = {
+            ['language_server.diagnostics_on_update'] = false,
+            ['language_server.diagnostics_on_open'] = false,
+            ['language_server.diagnostics_on_save'] = false,
+            ['language_server_phpstan.enabled'] = false,
+            ['language_server_psalm.enabled'] = false,
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
