17801d1b05f7f1f89cd2f0293ec984ee42a7a856
cy <cyrod@hey.com>
2025-07-21 10:00:40 +0800

    WIP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -217,33 +217,49 @@ return {
             },
           },
         },
-        -- intelephense = {},
+        intelephense = {
+          commands = {
+            IntelephenseIndex = {
+              function()
+                vim.lsp.buf.execute_command { command = 'intelephense.index.workspace' }
+              end,
+            },
+          },
+          on_attach = function(client, bufnr)
+            -- client.server_capabilities.documentFormattingProvider = false
+            -- client.server_capabilities.documentRangeFormattingProvider = false
+            -- if client.server_capabilities.inlayHintProvider then
+            --   vim.lsp.buf.inlay_hint(bufnr, true)
+            -- end
+          end,
+          capabilities = capabilities,
+        },
         svelte = {},
         tailwindcss = {},
-        phpactor = {
-          capabilities = capabilities,
-          on_init = function(client, bufnr)
-            client.server_capabilities.implementationProvider = false
-            client.server_capabilities.referencesProvider = false
-            client.server_capabilities.renameProvider = false
-            client.server_capabilities.selectionRangeProvider = false
-            client.server_capabilities.signatureHelpProvider = false
-            client.server_capabilities.typeDefinitionProvider = false
-            client.server_capabilities.workspaceSymbolProvider = false
-            client.server_capabilities.definitionProvider = false
-            client.server_capabilities.documentHighlightProvider = false
-            client.server_capabilities.documentSymbolProvider = false
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
-          end,
-          init_options = {
-            ['language_server.diagnostics_on_update'] = false,
-            ['language_server.diagnostics_on_open'] = false,
-            ['language_server.diagnostics_on_save'] = false,
-            ['language_server_phpstan.enabled'] = false,
-            ['language_server_psalm.enabled'] = false,
-          },
-        },
+        --   phpactor = {
+        --     capabilities = capabilities,
+        --     on_init = function(client, bufnr)
+        --       client.server_capabilities.implementationProvider = false
+        --       client.server_capabilities.referencesProvider = false
+        --       client.server_capabilities.renameProvider = false
+        --       client.server_capabilities.selectionRangeProvider = false
+        --       client.server_capabilities.signatureHelpProvider = false
+        --       client.server_capabilities.typeDefinitionProvider = false
+        --       client.server_capabilities.workspaceSymbolProvider = false
+        --       client.server_capabilities.definitionProvider = false
+        --       client.server_capabilities.documentHighlightProvider = false
+        --       client.server_capabilities.documentSymbolProvider = false
+        --       client.server_capabilities.documentFormattingProvider = false
+        --       client.server_capabilities.documentRangeFormattingProvider = false
+        --     end,
+        --     init_options = {
+        --       ['language_server.diagnostics_on_update'] = false,
+        --       ['language_server.diagnostics_on_open'] = false,
+        --       ['language_server.diagnostics_on_save'] = false,
+        --       ['language_server_phpstan.enabled'] = false,
+        --       ['language_server_psalm.enabled'] = false,
+        --     },
+        --   },
       }
 
       -- Ensure the servers and tools above are installed
